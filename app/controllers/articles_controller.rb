3d8067529ddb6c645268db8d234532a34cd93801
daniyal <daniyal.saif@xprolabs.com>
2020-03-17 18:10:33 +0500

    Association of Models

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,11 +8,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
- def new
- 	@article = Article.new
- end
+  def edit
+  @article = Article.find(params[:id])
+  end
 
- def create
+  def new
+ 	@article = Article.new
+
+  end
+
+  def create
  		@article = Article.new(article_params)
         if @article.save
    			redirect_to @article
@@ -20,14 +25,31 @@ class ArticlesController < ApplicationController
     		render 'new'
     	end	
     	
- end
+  end
  		
- 	
+ 	def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  
+  end
+  
+  end
 
+
+  def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+  end
 
  	private
       def article_params
        params.require(:article).permit(:title, :text)
       end
 
-end
+  end
